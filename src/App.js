afda3fd7922aa2de4c34168b4f3020791a0da0b4
Luciano J Guerra <dsibr@hotmail.com>
2020-01-25 17:11:13 -0300

    ADD - Global styles

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
 
+// Global styles
+import GlobalStyles from './assets/styles/globals';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyles />
     </Router>
   );
 }
