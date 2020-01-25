db132833a5a728c246b9c7d6bc459ae339e01871
Luciano J Guerra <dsibr@hotmail.com>
2020-01-25 16:56:09 -0300

    ADD - Auth and Default Layouts

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+// Layouts
+import AuthLayout from '../pages/_Layouts/Auth';
+import DefaultLayout from '../pages/_Layouts/Default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
@@ -17,7 +21,18 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 /**
