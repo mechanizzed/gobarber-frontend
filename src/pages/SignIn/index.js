833c2c54cacd06906d8f10379d5360a832a43d8e
Luciano J Guerra <dsibr@hotmail.com>
2020-01-25 18:17:14 -0300

    ADD - unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import { Form, Input } from '@rocketseat/unform';
+
+// Logo
 import logo from '../../assets/images/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="goBarber" title="goBarber" />
 
-      <form>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
 
         <button type="submit">Entrar</button>
 
         <Link to="/register">Não tem uma conta? Registre-se agora</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import { Form, Input } from '@rocketseat/unform';
+
+// Logo
 import logo from '../../assets/images/logo.svg';
 
-// import { Container } from './styles';
-
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="goBarber" title="goBarber" />
 
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
 
         <button type="submit">Criar conta</button>
 
         <Link to="/">Já tenho uma conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
