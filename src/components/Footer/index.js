97f586e5101416d7cdee6485734f3b20eeebc2b2
Luciano J Guerra <dsibr@hotmail.com>
2020-01-27 21:38:40 -0300

    ADD - Layout

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Inside } from './styles';
+
+export default function Footer() {
+  return (
+    <Container>
+      <Inside>
+        <p>todos os direitos reservados</p>
+      </Inside>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #26262b;
+  padding: 20px;
+`;
+
+export const Inside = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 900px;
+  text-align: center;
+
+  p {
+    color: #fff;
+    font-size: 12px;
+  }
+`;
