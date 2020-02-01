04f81d1b2ec8ebb70eb8ef0c4877fcc92684bb36
Luciano J Guerra <dsibr@hotmail.com>
2020-02-01 19:21:37 -0300

    UPDATE - axios setup

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,17 @@
-import React, { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
+import React, { useState, useEffect, useMemo } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  setMilliseconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
 
 // icons
@@ -9,19 +21,53 @@ import {
   MdChevronRight,
   MdDateRange,
 } from 'react-icons/md';
-import api from '../../services/api';
 
-// icons
+import api from '../../services/api';
 
 import { Container, Title, Header, TimeContainer, TimeCard } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
   const [date, setDate] = useState(new Date());
+  const [schedules, setSchedules] = useState([]);
 
   const dateFormatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
     [date]
   );
+
+  useEffect(() => {
+    async function loadSchedules() {
+      const response = await api.get('schedules', {
+        params: { date },
+        // params: { date: '2020-01-28T02:56:58.604Z' },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        // const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+
+      setSchedules(data);
+    }
+
+    loadSchedules();
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -50,22 +96,20 @@ export default function Dashboard() {
       </Header>
 
       <TimeContainer>
-        <TimeCard past>
-          <strong>08:00</strong>
-          <span>Reservado por: Luciano</span>
-        </TimeCard>
-        <TimeCard available>
-          <strong>09:00</strong>
-          <span>em aberto</span>
-        </TimeCard>
-        <TimeCard>
-          <strong>10:00</strong>
-          <span>Reservado por: Luciano</span>
-        </TimeCard>
-        <TimeCard>
-          <strong>11:00</strong>
-          <span>Reservado por: Luciano</span>
-        </TimeCard>
+        {schedules.map(schedule => (
+          <TimeCard
+            key={schedule.id}
+            past={schedule.past}
+            available={!schedule.appointment}
+          >
+            <strong>{schedule.time}</strong>
+            <span>
+              {schedule.appointment
+                ? `Reservado por: ${schedule.appointment.user.name}`
+                : 'Em aberto'}
+            </span>
+          </TimeCard>
+        ))}
       </TimeContainer>
     </Container>
   );
